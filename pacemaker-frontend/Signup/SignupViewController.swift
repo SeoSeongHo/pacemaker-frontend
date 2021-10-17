f782a4fddbd9abd97328d9929571066620e6e448
ydh1304 <donghyun.lee@mvlchain.io>
2021-10-17 20:23:16 +0900

    Signup flow

---
+++ pacemaker-frontend/Signup/SignupViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignupViewController.swift
+//  pacemaker-frontend
+//
+//  Created by 이동현 on 2021/10/17.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        view.backgroundColor = .white
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
